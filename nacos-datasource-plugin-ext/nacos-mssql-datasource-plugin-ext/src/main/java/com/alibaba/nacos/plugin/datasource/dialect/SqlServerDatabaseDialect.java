280bafe53f55fdbc036e57d1af7395b4c54fc2f8
QY Li <m13386474590@163.com>
2024-03-23 02:21:47 +0800

    添加 Microsoft SQL Server 支持

---
+++ nacos-datasource-plugin-ext/nacos-mssql-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/dialect/SqlServerDatabaseDialect.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 1999-2022 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.datasource.dialect;
+
+import com.alibaba.nacos.plugin.datasource.constants.DatabaseTypeConstant;
+
+/**
+ * Microsoft SQL Server database dialect.
+ * 务必确保表的主键为id。
+ * Always make sure that the primary key of the table is "id".
+ * @author QY Li
+ */
+public abstract class SqlServerDatabaseDialect extends AbstractDatabaseDialect {
+    @Override
+    public String getType() {
+        return DatabaseTypeConstant.SQLSERVER;
+    }
+
+	@Override
+    public String getLimitTopSqlWithMark(String sql) {
+        return sql + " ORDER BY id OFFSET 0 ROWS FETCH NEXT ? ROWS ONLY ";
+    }
+
+    @Override
+    public String getLimitPageSqlWithMark(String sql) {
+        return sql + " ORDER BY id OFFSET ? ROWS FETCH NEXT ? ROWS ONLY ";
+    }
+
+    @Override
+    public String getLimitPageSql(String sql, int pageNo, int pageSize) {
+        return sql + " ORDER BY id OFFSET " + getPagePrevNum(pageNo, pageSize)
+                + " ROWS FETCH NEXT " + pageSize + " ROWS ONLY ";
+    }
+    
+    @Override
+    public String getLimitPageSqlWithOffset(String sql, int startOffset, int pageSize){
+        return sql + " ORDER BY id OFFSET " + startOffset + " ROWS FETCH NEXT "
+                + pageSize + " ROWS ONLY ";
+    }
+}
---
+++ nacos-datasource-plugin-ext/nacos-mssql-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/mssql/TenantCapacityMapperBySqlServer.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 1999-2022 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.datasource.impl.mssql;
+
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.alibaba.nacos.plugin.datasource.constants.DatabaseTypeConstant;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
+import com.alibaba.nacos.plugin.datasource.impl.base.BaseTenantCapacityMapper;
+import com.alibaba.nacos.plugin.datasource.model.MapperContext;
+import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+
+/**
+ * The SQL Server implementation of TenantCapacityMapper.
+ *
+ * @author QY Li
+ **/
+public class TenantCapacityMapperBySqlServer extends BaseTenantCapacityMapper {
+
+    @Override
+    public MapperResult getCapacityList4CorrectUsage(MapperContext context) {
+        String sql = "SELECT id, tenant_id FROM tenant_capacity WHERE id > ? ORDER BY id "
+                + " OFFSET 0 ROWS FETCH NEXT ? ROWS ONLY ";
+        return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.ID),
+                context.getWhereParameter(FieldConstant.LIMIT_SIZE)));
+    }
+
+    @Override
+    public String getDataSource() {
+        return DatabaseTypeConstant.SQLSERVER;
+    }
+
+}
