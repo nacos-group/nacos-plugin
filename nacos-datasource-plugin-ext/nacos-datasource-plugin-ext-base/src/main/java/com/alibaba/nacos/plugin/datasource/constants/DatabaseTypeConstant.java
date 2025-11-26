208dcffa2ef57f8bb68cb02ec65dc0ed64574e0c
huhengliang <huhengliang@sics.ac.cn>
2025-11-26 10:40:39 +0800

    fix: Rename the database

--- nacos-datasource-plugin-ext/nacos-datasource-plugin-ext-base/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DatabaseTypeConstant.java
+++ nacos-datasource-plugin-ext/nacos-datasource-plugin-ext-base/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DatabaseTypeConstant.java
@@ -37,6 +37,6 @@ public class DatabaseTypeConstant {
 
     public static final String KINGBASE = "kingbase";
 
-    public static final String YASHAN = "yashan";
+    public static final String YASHAN = "yasdb";
 
 }
