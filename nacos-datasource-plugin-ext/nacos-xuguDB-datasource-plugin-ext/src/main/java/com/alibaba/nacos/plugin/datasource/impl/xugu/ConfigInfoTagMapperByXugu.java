299932e4e809c88e79a2c161f2461071b9c8b9bd
zhaoyangplus <1410056984@qq.com>
2026-03-10 15:26:14 +0800

    feat(plugin): Added support for the xuguDB database (#118)
    * feat(plugin): Added support for the xuguDB database
    
    * (fix):Rollback the erroneous submission caused by merge conflicts

--- nacos-datasource-plugin-ext/nacos-xuguDB-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/xugu/ConfigInfoTagMapperByXugu.java
+++ nacos-datasource-plugin-ext/nacos-xuguDB-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/xugu/ConfigInfoTagMapperByXugu.java
@@ -14,31 +14,22 @@
  * limitations under the License.
  */
 
-package com.alibaba.nacos.plugin.datasource.constants;
+package com.alibaba.nacos.plugin.datasource.impl.xugu;
+
+import com.alibaba.nacos.plugin.datasource.constants.DatabaseTypeConstant;
+import com.alibaba.nacos.plugin.datasource.impl.base.BaseConfigInfoTagMapper;
 
 /**
- * DatabaseType Constant.
+ * The xuguDB implementation of ConfigInfoTagMapperByXugu.
  *
- * @author Long Yu
+ * @author jowee
  **/
-public class DatabaseTypeConstant {
 
-    public static final String POSTGRESQL = "postgresql";
+public class ConfigInfoTagMapperByXugu extends BaseConfigInfoTagMapper {
 
-    public static final String GUASSDB = "gaussdb";
-
-    public static final String MYSQL = "mysql";
-
-    public static final String ORACLE = "oracle";
-  
-    public static final String DM = "dm";
-
-    public static final String SQLSERVER = "sqlserver";
-
-    public static final String KINGBASE = "kingbase";
-
-    public static final String YASDB = "yasdb";
-
-    public static final String XUGU = "xugu";
+    @Override
+    public String getDataSource() {
+        return DatabaseTypeConstant.XUGU;
+    }
 
 }
