57b9110cbd86514cdf7ad6fa643990f6957d0b7f
zhaoyangplus <1410056984@qq.com>
2026-03-19 10:33:59 +0800

    fix(xugu): 1. Add the missing fields to the his_config_info table. 2. Remove the JDBC coordinates in the pom.xml because introducing the startup will cause a logback conflict. You can directly add the JDBC JAR in the plugins folder.

--- nacos-datasource-plugin-ext/nacos-xuguDB-datasource-plugin-ext/src/main/resources/schema/nacos-xugu.sql
+++ nacos-datasource-plugin-ext/nacos-xuguDB-datasource-plugin-ext/src/main/resources/schema/nacos-xugu.sql
@@ -182,6 +182,9 @@ CREATE TABLE `his_config_info`
     `op_type`            char(10)               DEFAULT NULL COMMENT 'operation type',
     `tenant_id`          varchar(128)           DEFAULT '' COMMENT '租户字段',
     `encrypted_data_key` varchar(1024) NOT NULL DEFAULT '' COMMENT '密钥',
+    `publish_type` varchar(50)  DEFAULT 'formal' COMMENT 'publish type gray or formal',
+    `gray_name` varchar(50)  DEFAULT NULL COMMENT 'gray name',
+    `ext_info`  longtext DEFAULT NULL COMMENT 'ext info',
     PRIMARY KEY (`nid`)
 )COMMENT '多租户改造';
 create index `idx_did` on `his_config_info` (`data_id`);
