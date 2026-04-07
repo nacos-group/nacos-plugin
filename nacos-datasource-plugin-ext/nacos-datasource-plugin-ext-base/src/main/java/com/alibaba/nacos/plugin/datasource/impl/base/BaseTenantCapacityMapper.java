1727e65d528241d6ba0ac75f8d3068d681356edb
Ken <kan.zhang-cn@hotmail.com>
2026-04-07 16:01:55 +0800

    refactor(datasource-base): 补充默认实现方法(MySQL)，便于后面的插件在语法相同情况下重复编写

--- nacos-datasource-plugin-ext/nacos-datasource-plugin-ext-base/src/main/java/com/alibaba/nacos/plugin/datasource/impl/base/BaseTenantCapacityMapper.java
+++ nacos-datasource-plugin-ext/nacos-datasource-plugin-ext-base/src/main/java/com/alibaba/nacos/plugin/datasource/impl/base/BaseTenantCapacityMapper.java
@@ -18,12 +18,16 @@ package com.alibaba.nacos.plugin.datasource.impl.base;
 
 import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
+import com.alibaba.nacos.plugin.datasource.constants.TableConstant;
 import com.alibaba.nacos.plugin.datasource.dialect.DatabaseDialect;
 import com.alibaba.nacos.plugin.datasource.manager.DatabaseDialectManager;
 import com.alibaba.nacos.plugin.datasource.mapper.AbstractMapper;
 import com.alibaba.nacos.plugin.datasource.mapper.TenantCapacityMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The base implementation of TenantCapacityMapper.
@@ -39,11 +43,80 @@ public abstract class BaseTenantCapacityMapper extends AbstractMapper implements
     }
     
     @Override
+    public MapperResult select(MapperContext context) {
+        String sql = "select id,tenant_id,quota,`usage`,max_size,max_aggr_count,max_aggr_size "
+                + " from tenant_capacity where tenant_id = ?";
+        return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.TENANT_ID)));
+    }
+    
+    @Override
+    public MapperResult incrementUsageWithDefaultQuotaLimit(MapperContext context) {
+        String sql = "update tenant_capacity set `usage` = `usage` + 1, gmt_modified = ? "
+                + " where tenant_id = ? and `usage` < ? and quota = 0";
+        return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.GMT_MODIFIED),
+                context.getWhereParameter(FieldConstant.TENANT_ID), context.getWhereParameter(FieldConstant.USAGE)));
+    }
+    
+    @Override
+    public MapperResult incrementUsageWithQuotaLimit(MapperContext context) {
+        String sql = "update tenant_capacity set `usage` = `usage` + 1, gmt_modified = ? "
+                + " where tenant_id = ? and `usage` < quota and quota != 0";
+        return new MapperResult(sql, CollectionUtils.list(context.getUpdateParameter(FieldConstant.GMT_MODIFIED),
+                context.getWhereParameter(FieldConstant.TENANT_ID)));
+    }
+    
+    @Override
+    public MapperResult incrementUsage(MapperContext context) {
+        String sql = "UPDATE tenant_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE tenant_id = ?";
+        return new MapperResult(sql, CollectionUtils.list(context.getUpdateParameter(FieldConstant.GMT_MODIFIED),
+                context.getWhereParameter(FieldConstant.TENANT_ID)));
+    }
+    
+    @Override
+    public MapperResult decrementUsage(MapperContext context) {
+        String sql = "UPDATE tenant_capacity SET `usage` = `usage` - 1, gmt_modified = ? WHERE tenant_id = ? AND `usage` > 0";
+        return new MapperResult(sql, CollectionUtils.list(context.getUpdateParameter(FieldConstant.GMT_MODIFIED),
+                context.getWhereParameter(FieldConstant.TENANT_ID)));
+    }
+    
+    @Override
+    public MapperResult correctUsage(MapperContext context) {
+        String sql = "UPDATE tenant_capacity SET `usage` = (SELECT count(*) FROM config_info WHERE tenant_id = ?), "
+                        + "gmt_modified = ? WHERE tenant_id = ?";
+        return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.TENANT_ID),
+                context.getUpdateParameter(FieldConstant.GMT_MODIFIED), context.getWhereParameter(FieldConstant.TENANT_ID)));
+    }
+    
+    @Override
     public MapperResult getCapacityList4CorrectUsage(MapperContext context) {
         String sql = databaseDialect.getLimitTopSqlWithMark("SELECT id, tenant_id FROM tenant_capacity WHERE id>?");
         return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.ID),
                 context.getWhereParameter(FieldConstant.LIMIT_SIZE)));
     }
+    
+    @Override
+    public MapperResult insertTenantCapacity(MapperContext context) {
+        List<Object> paramList = new ArrayList<>();
+        paramList.add(context.getUpdateParameter(FieldConstant.TENANT_ID));
+        paramList.add(context.getUpdateParameter(FieldConstant.QUOTA));
+        paramList.add(context.getUpdateParameter(FieldConstant.MAX_SIZE));
+        paramList.add(context.getUpdateParameter(FieldConstant.MAX_AGGR_COUNT));
+        paramList.add(context.getUpdateParameter(FieldConstant.MAX_AGGR_SIZE));
+        paramList.add(context.getUpdateParameter(FieldConstant.GMT_CREATE));
+        paramList.add(context.getUpdateParameter(FieldConstant.GMT_MODIFIED));
+        paramList.add(context.getWhereParameter(FieldConstant.TENANT_ID));
+        
+        return new MapperResult(
+                "INSERT INTO tenant_capacity (tenant_id, quota, `usage`, max_size, max_aggr_count, max_aggr_size, "
+                        + "gmt_create, gmt_modified) SELECT ?, ?, count(*), ?, ?, ?, ?, ? FROM config_info WHERE tenant_id=?",
+                paramList);
+    }
+    
+    @Override
+    public String getTableName() {
+        return TableConstant.TENANT_CAPACITY;
+    }
+    
     @Override
     public String getFunction(String functionName) {
         return databaseDialect.getFunction(functionName);
