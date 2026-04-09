626fa50994a25f89ff141c92ba22a073d82ee374
Ken <kan.zhang-cn@hotmail.com>
2026-04-09 11:28:17 +0800

    feat(openGauss): 适配3.2.0版本 (#137)
    新增表
    + pipeline_execution
    + ai_resource
    + ai_resource_version
    
    Close #136

---
+++ nacos-datasource-plugin-ext/nacos-opengauss-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/opengauss/OpenGaussAiResourceMapper.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1999-2026 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.datasource.impl.opengauss;
+
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
+import com.alibaba.nacos.plugin.datasource.mapper.AiResourceMapper;
+import com.alibaba.nacos.plugin.datasource.mapper.ext.WhereBuilder;
+import com.alibaba.nacos.plugin.datasource.model.MapperContext;
+import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+
+/**
+ * The base implementation of AiResourceMapper.
+ *
+ * @author Ken
+ */
+public class OpenGaussAiResourceMapper extends AbstractMapperByGaussdb implements AiResourceMapper {
+    
+    @Override
+    public MapperResult findAiResourceFetchRows(MapperContext context) {
+        WhereBuilder where = new WhereBuilder(
+        "SELECT id,gmt_create,gmt_modified,name,type,c_desc,status,namespace_id,"
+                + "biz_tags,ext,version_info,meta_version,scope,owner,download_count "
+                + "FROM ai_resource");
+        where.eq("namespace_id", context.getWhereParameter(FieldConstant.NAMESPACE_ID));
+
+        appendExtraQueryCondition(where, context);
+
+        MapperResult built = where.build();
+        String sql = built.getSql() + resolveOrderByClause(context) + " LIMIT " + context.getPageSize() + " OFFSET "
+                + context.getStartRow();
+        return new MapperResult(sql, built.getParamList());
+    }
+    
+}
