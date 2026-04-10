fa5a9ec990b22e24fb6a70a92124f9960c539e9b
Ken <kan.zhang-cn@hotmail.com>
2026-04-10 13:50:01 +0800

    feat(DM): 适配 3.2.x (#145)

--- nacos-datasource-plugin-ext/nacos-dm-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/dm/ConfigInfoGrayMapperByDaMeng.java
+++ nacos-datasource-plugin-ext/nacos-dm-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/dm/ConfigInfoGrayMapperByDaMeng.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2022 Alibaba Group Holding Ltd.
+ * Copyright 1999-2026 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,21 +18,33 @@ package com.alibaba.nacos.plugin.datasource.impl.dm;
 
 import com.alibaba.nacos.plugin.datasource.constants.DatabaseTypeConstant;
 import com.alibaba.nacos.plugin.datasource.constants.PrimaryKeyConstant;
-import com.alibaba.nacos.plugin.datasource.impl.base.BaseTenantInfoMapper;
-import com.alibaba.nacos.plugin.datasource.mapper.TenantInfoMapper;
+import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoGrayMapper;
+import com.alibaba.nacos.plugin.datasource.model.MapperContext;
+import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+
+import java.util.Collections;
 
 /**
- * The dameng implementation of ConfigInfoAggrMapper.
- *
- * @author Xiao Yong
- **/
-public class TenantInfoMapperByDaMeng extends AbstractMapperByDaMeng implements TenantInfoMapper {
+ * @author Ken
+ */
+public class ConfigInfoGrayMapperByDaMeng extends AbstractMapperByDaMeng implements ConfigInfoGrayMapper {
 
+    @Override
+    public MapperResult findAllConfigInfoGrayForDumpAllFetchRows(MapperContext context) {
+        String sql = getLimitPageSqlWithOffset("SELECT id,data_id,group_id,tenant_id,gray_name,app_name,content,md5,gmt_modified "
+                        +" from config_info_gray ORDER BY id ", context.getStartRow(), context.getPageSize());
+        return new MapperResult(sql, Collections.emptyList());
+    }
+    
+    private String getLimitPageSqlWithOffset(String sql, int offset, int limit) {
+        return getDialect().getLimitPageSqlWithOffset(sql,offset,limit);
+    }
+    
     @Override
     public String getDataSource() {
         return DatabaseTypeConstant.DM;
     }
-
+    
     @Override
     public String[] getPrimaryKeyGeneratedKeys() {
         return PrimaryKeyConstant.UPPER_RETURN_PRIMARY_KEYS;
